a06e6f281ef8f1a948f256ba3e7782b7f16067b8
Neto sergey test <serez.gavriliv@mail.ru>
2024-02-13 10:25:06 +0300

    initial

--- src/test/java/ru/netology/services/QAdz5/FreeServiceTest.java
+++ src/test/java/ru/netology/services/QAdz5/FreeServiceTest.java
@@ -1,10 +1,9 @@
 package ru.netology.services.QAdz5;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+
 
 public class FreeServiceTest {
     @ParameterizedTest
